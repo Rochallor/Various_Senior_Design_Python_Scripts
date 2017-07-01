97ba7c53a94c07777af74c835254e835603ad0cc
Rochallor <khira.morgan@gmail.com>
2017-06-30 21:07:52 -0500

    Add files via upload

---
+++ capture.py
@@ -0,0 +1,26 @@
+#==============================================================================
+# File Name: 	capture
+# Author: 	   Khira Morgan
+# Project:	   Senior Design Fall 2017
+# 
+# Description: This function will analyze a move to capture, update the new 
+#		         board positions from the old board positions, and return the 
+#              updated board.
+# Inputs: 
+# 	       i.   oldboard = 2D list of old board positions
+# 	       ii.  new_blank is a 1D list with two elements designating the square
+#		         that the piece was moved from -- occupied->space
+# 	       iii. new_occupied is a 1D list with two elements designating the 
+#              square that the piece was moved to -- space->occupied
+#==============================================================================
+
+def capture(oldboard, new_blank, new_occupied):
+
+    newboard = oldboard[:];
+    
+    temp = oldboard[new_blank[0]][new_blank[1]];
+
+    newboard[new_blank[0]][new_blank[1]] = 's';
+    newboard[new_occupied[0]][new_occupied[1]] = temp;
+    
+    return newboard;
